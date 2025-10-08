955c60615ebfb5c019529e0f8d15b14a54712d0c
Useit14 <useit14@inbox.ru>
2025-10-08 17:06:52 +0300

    changes

--- apps/ctlog/src/App.tsx
+++ apps/ctlog/src/App.tsx
@@ -1,11 +1,11 @@
-import i18next from "i18next";
 import { HomePage } from "./pages/HomePage";
 import { StoreReduxProvider } from "./redux/ui/StoreProvider";
 import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n"; // Важно импортировать именно i18n, а не i18next
 
 function App() {
   return (
-    <I18nextProvider i18n={i18next}>
+    <I18nextProvider i18n={i18n}>
       <StoreReduxProvider>
         <HomePage />
       </StoreReduxProvider>
