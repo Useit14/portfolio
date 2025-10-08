1f6698b7cadcae9ef765b6bb6550ce5e9ef6390b
Useit14 <useit14@inbox.ru>
2025-10-08 16:53:10 +0300

    add i18

--- apps/ctlog/src/App.tsx
+++ apps/ctlog/src/App.tsx
@@ -1,11 +1,15 @@
+import i18next from "i18next";
 import { HomePage } from "./pages/HomePage";
 import { StoreReduxProvider } from "./redux/ui/StoreProvider";
+import { I18nextProvider } from "react-i18next";
 
 function App() {
   return (
-    <StoreReduxProvider>
-      <HomePage />
-    </StoreReduxProvider>
+    <I18nextProvider i18n={i18next}>
+      <StoreReduxProvider>
+        <HomePage />
+      </StoreReduxProvider>
+    </I18nextProvider>
   );
 }
 
